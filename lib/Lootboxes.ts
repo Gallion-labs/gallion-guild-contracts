575fb4fb218d9991c3988e103c573379494e7b22
Luc Labbé <luc.labbe@gmail.com>
2022-08-24 19:22:25 +0200

    feat(lib): add lootboxes listing

--- lib/Lootboxes.ts
+++ lib/Lootboxes.ts
@@ -11,4 +11,34 @@ export default abstract class Lootboxes extends BasicFacetWrapper {
         const tx = await contract.open(playerAddress, lootboxId, gasStationData);
         await tx.wait();
     }
+
+    public static async list(contractAddress: string, playerAddress: string): Promise<LootboxesCount> {
+        const contract = super.getContractFacet(contractAddress, this.ABI) as LootboxFacet;
+        const gasStationData: GasStationData = await GasStation.getGasData();
+        const lootboxes = await contract.list(playerAddress, gasStationData);
+        if (lootboxes.length === 5) {
+            return {
+                Common: lootboxes[0].toNumber(),
+                Rare: lootboxes[1].toNumber(),
+                Epic: lootboxes[2].toNumber(),
+                Legendary: lootboxes[3].toNumber(),
+                Ethereal: lootboxes[4].toNumber(),
+            };
+        }
+        return {
+            Common: 0,
+            Rare: 0,
+            Epic: 0,
+            Legendary: 0,
+            Ethereal: 0,
+        };
+    }
+}
+
+export interface LootboxesCount {
+    Common: number;
+    Rare: number;
+    Epic: number;
+    Legendary: number;
+    Ethereal: number;
 }
