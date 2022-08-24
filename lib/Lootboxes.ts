ca16c5bd6977979dcd82911342e0b23e62c58976
Luc Labbé <luc.labbe@gmail.com>
2022-08-24 18:51:33 +0200

    fix(lib): await transaction call

--- lib/Lootboxes.ts
+++ lib/Lootboxes.ts
@@ -1,11 +1,14 @@
 import BasicFacetWrapper from "./BasicFacetWrapper";
-import { LootboxFacet__factory, PlayerFacet__factory, LootboxFacet } from "../typechain-types";
+import { LootboxFacet, LootboxFacet__factory } from "../typechain-types";
+import { GasStation, GasStationData } from "../scripts/gasStation";
 
 export default abstract class Lootboxes extends BasicFacetWrapper {
     private static readonly ABI = LootboxFacet__factory.abi;
 
     public static async open(contractAddress: string, playerAddress: string, lootboxId: number): Promise<void> {
         const contract = super.getContractFacet(contractAddress, this.ABI) as LootboxFacet;
-        await contract.open(playerAddress, lootboxId);
+        const gasStationData: GasStationData = await GasStation.getGasData();
+        const tx = await contract.open(playerAddress, lootboxId, gasStationData);
+        await tx.wait();
     }
 }
--- lib/Players.ts
+++ lib/Players.ts
@@ -1,11 +1,21 @@
 import BasicFacetWrapper from "./BasicFacetWrapper";
 import { PlayerFacet, PlayerFacet__factory } from "../typechain-types";
+import { GasStation, GasStationData } from "../scripts/gasStation";
 
 export default abstract class Players extends BasicFacetWrapper {
     private static readonly ABI = PlayerFacet__factory.abi;
 
     public static async add(contractAddress: string, playerAddress: string): Promise<void> {
         const contract = super.getContractFacet(contractAddress, this.ABI) as PlayerFacet;
-        await contract.addPlayer(playerAddress);
+        const gasStationData: GasStationData = await GasStation.getGasData();
+        const tx = await contract.addPlayer(playerAddress, gasStationData);
+        await tx.wait();
+    }
+
+    public static async levelUp(contractAddress: string, playerAddress: string): Promise<void> {
+        const contract = super.getContractFacet(contractAddress, this.ABI) as PlayerFacet;
+        const gasStationData: GasStationData = await GasStation.getGasData();
+        const tx = await contract.levelUp(playerAddress, gasStationData);
+        await tx.wait();
     }
 }
