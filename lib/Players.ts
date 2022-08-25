924f2bdee75b6fdb8e2aee8c74ff525f619ca4ca
Florian <contact@florianleger.fr>
2022-08-25 20:42:32 +0200

    fix(sdk): add getBalances function in Player lib

--- lib/Players.ts
+++ lib/Players.ts
@@ -1,9 +1,22 @@
-import BasicFacetWrapper from "./BasicFacetWrapper";
-import { PlayerFacet, PlayerFacet__factory } from "../typechain-types";
-import { GasStation, GasStationData } from "../scripts/gasStation";
+import BasicFacetWrapper from './BasicFacetWrapper';
+import { PlayerFacet, PlayerFacet__factory } from '../typechain-types';
+import { GasStation, GasStationData } from '../scripts/gasStation';
 
-export default abstract class Players extends BasicFacetWrapper {
+export abstract class Players extends BasicFacetWrapper {
     private static readonly ABI = PlayerFacet__factory.abi;
+
+    public static async getBalances(contractAddress: string, playerAddress: string): Promise<PlayerBalances> {
+        const contract = super.getContractFacet(contractAddress, this.ABI) as PlayerFacet;
+        const playerStats: PlayerFacet.PlayerInfoStructOutput = await contract.player(playerAddress);
+        return {
+            guildTokens: playerStats.balances[0].toNumber(),
+            commonLootboxes: playerStats.balances[1].toNumber(),
+            rareLootboxes: playerStats.balances[2].toNumber(),
+            epicLootboxes: playerStats.balances[3].toNumber(),
+            legendaryLootboxes: playerStats.balances[4].toNumber(),
+            etherealLootboxes: playerStats.balances[5].toNumber()
+        };
+    }
 
     public static async add(contractAddress: string, playerAddress: string): Promise<void> {
         const contract = super.getContractFacet(contractAddress, this.ABI) as PlayerFacet;
@@ -18,4 +31,13 @@ export default abstract class Players extends BasicFacetWrapper {
         const tx = await contract.levelUp(playerAddress, gasStationData);
         await tx.wait();
     }
+}
+
+export interface PlayerBalances {
+    guildTokens: number;
+    commonLootboxes: number;
+    rareLootboxes: number;
+    epicLootboxes: number;
+    legendaryLootboxes: number;
+    etherealLootboxes: number;
 }
